0ad586ffb3948cd4d0aee9fbe53c9a87a2aaab55
Lourdes Prinster <sinvita@rocketmail.com>
2014-05-02 20:26:04 -0600

    Customer validations

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -7,9 +7,11 @@ class Customer < ActiveRecord::Base
   validates :city,          length: { minimum: 1, message: "can't be blank" }
   validates :state,         length: { is: 2 }
   validates :zipcode,       format: { with: /\A\d{5}\z/, message: "should be 5 digits" }
-  validates :phone1,        length: { is: 10 }
-#   validates :phone2,        length: { is: 10 }, allow_nil: true       
-  validates :email,         presence: true
+  validates :phone1,        length: { is: 12 }
+  validates :phone1,        format: { with: /\d{3}-\d{3}-\d{4}/, message: "bad format" }
+  validates :phone2,        length: { is: 12 }, allow_blank: true 
+        
+  validates :email,         format: { with: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i, on: :create }
 #   validates :cc_type,       presence: true
 #   validates :cc_number1,    length: { minimum: 4, message: "can't be blank" }
 #   validates :cc_number2,    length: { minimum: 4, message: "can't be blank" }
